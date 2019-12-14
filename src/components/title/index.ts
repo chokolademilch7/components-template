b2ed2f2e66a4b661f83bba66f1a09826a6a298cb
Daiki <ruman.orchid@gmail.com>
2019-12-14 13:09:18 +0900

    Update: 型定義追加

--- src/components/title/index.ts
+++ src/components/title/index.ts
@@ -1,9 +1,13 @@
 import {
-  LitElement, html, customElement
+  LitElement, html, customElement, property
 } from 'lit-element';
+import style from './index.gen.css'
 
 @customElement('a100-title')
 export class a100Title extends LitElement {
+  @property({type: String}) name = '';
+
+  static get styles() {return [style]}
   render() {
     return html`
       <div>これはタイトル</div>
